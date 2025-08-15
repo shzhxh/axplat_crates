fdd7ba382b37935d8e58fd4975a51620320840c1
Asakura Mizu <asakuramizu111@gmail.com>
2025-08-15 14:38:08 +0800

    cleanup

--- platforms/axplat-riscv64-qemu-virt/src/boot.rs
+++ platforms/axplat-riscv64-qemu-virt/src/boot.rs
@@ -67,9 +67,8 @@ unsafe extern "C" fn _start() -> ! {
 /// The earliest entry point for secondary CPUs.
 #[cfg(feature = "smp")]
 #[unsafe(naked)]
-#[unsafe(no_mangle)]
 #[unsafe(link_section = ".text.boot")]
-unsafe extern "C" fn _start_secondary() -> ! {
+pub(crate) unsafe extern "C" fn _start_secondary() -> ! {
     // a0 = hartid
     // a1 = SP
     core::arch::naked_asm!("
--- platforms/axplat-riscv64-qemu-virt/src/power.rs
+++ platforms/axplat-riscv64-qemu-virt/src/power.rs
@@ -12,14 +12,11 @@ impl PowerIf for PowerImpl {
     #[cfg(feature = "smp")]
     fn cpu_boot(cpu_id: usize, stack_top_paddr: usize) {
         use axplat::mem::{va, virt_to_phys};
-        unsafe extern "C" {
-            fn _start_secondary();
-        }
         if sbi_rt::probe_extension(sbi_rt::Hsm).is_unavailable() {
             warn!("HSM SBI extension is not supported for current SEE.");
             return;
         }
-        let entry = virt_to_phys(va!(_start_secondary as usize));
+        let entry = virt_to_phys(va!(crate::boot::_start_secondary as usize));
         sbi_rt::hart_start(cpu_id, entry.as_usize(), stack_top_paddr);
     }
 
